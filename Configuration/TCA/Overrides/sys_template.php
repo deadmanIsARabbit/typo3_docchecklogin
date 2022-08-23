09693184c750195a2946a89e2e6b9c543fb84599
Sabrina Zwirner <sabrina.zwirner@antwerpes.com>
2022-08-23 12:14:53 +0200

    init

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3') || die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('typo3_docchecklogin', 'Configuration/TypoScript', 'typo3-docchecklogin');
---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'typo3-docchecklogin',
+    'description' => '',
+    'category' => 'plugin',
+    'author' => 'Sabrina Zwirner, Michael Paffrath',
+    'author_email' => 'sabrina.zwirner@antwerpe.com, michael.paffrath@antwerpes.com',
+    'state' => 'alpha',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
